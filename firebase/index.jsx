abc1c245cd59e325509338a2a5565acfd6496ca3
Pavel Zapletal <pavel.zaplet25@gmail.com>
2021-10-29 22:08:08 +0200

    dd

--- firebase/index.jsx
+++ firebase/index.jsx
@@ -8,14 +8,15 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC4TDoJuZBfcwJzBAKbmZTmfinQczDlIg8",
-  authDomain: "fir-nextjs-todo.firebaseapp.com",
-  databaseURL: "https://fir-nextjs-todo-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "fir-nextjs-todo",
-  storageBucket: "fir-nextjs-todo.appspot.com",
-  messagingSenderId: "372167901298",
-  appId: "1:372167901298:web:a987b3e552e24d4a2842eb",
-  measurementId: "G-EE7HHDDDGS"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DB_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORRAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+
 };
 
 // Initialize Firebase
--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  env:{
+    API_KEY:process.env.NEXT_PUBLIC_API_KEY,
+    AUTH_DOMAIN: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    DB_URL: process.env.NEXT_PUBLIC_DB_URL,
+    PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID,
+    STORAGE_BUCKET: process.env.NEXT_PUBLIC_STORRAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    APP_ID: process.env.NEXT_PUBLIC_APP_ID,
+    MEASUREMENT_ID: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+  }
 }
