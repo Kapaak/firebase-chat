faffba25e5a6a76417cae334c49776223d20bf42
Pavel Zapletal <pavel.zaplet25@gmail.com>
2021-10-31 09:08:41 +0100

    dd

---
+++ pages/sign-in/index.jsx
@@ -0,0 +1,10 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const SignIn = () =>{
+    return(
+        <div>
+            <h1>Sign in here:</h1>
+        </div>
+    )
+}
+export default SignIn
